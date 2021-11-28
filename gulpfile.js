d08ca149876246fefcb5c31c3778960e1a187b8b
denis farkas <dfarkas960@gmail.com>
2021-11-28 10:04:51 +0100

    flow gulp for webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
-
 const { src, dest, watch, series }= require('gulp');
+
+//CSS et SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+//Images
+const imagemin = require('gulp-imagemin');
+const webp =require('gulp-webp');
 
 function css(done) {
     //compiler sass
@@ -17,10 +22,25 @@ function css(done) {
 
 }
 
+function images() {
+   
+    return src('src/img/**/*')
+    .pipe( imagemin({ optimizationLevel: 3 }))
+    .pipe( dest('build/img') );
+}
+
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+    .pipe( webp())
+    .pipe( dest('build/img') )
+}
 function dev(){
     watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', images);
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default =series( css, dev );
+exports.images = images;
+exports.versionWebp = versionWebp;
+exports.default =series( images,versionWebp, css, dev );
